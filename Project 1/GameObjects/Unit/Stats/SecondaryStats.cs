c8a55a72fc4fec3db1e82b20684db26ae7134b37
Cassandra Åkerberg <disa_cassandra@outlook.com>
2025-10-31 15:53:00 +0100

    Started implementation of damage changing

---
+++ Project 1/GameObjects/Unit/Stats/SecondaryStats.cs
@@ -0,0 +1,81 @@
+﻿using Project_1.GameObjects.Entities;
+using Project_1.GameObjects.Spawners;
+using Project_1.Items.SubTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_1.GameObjects.Unit.Stats
+{
+    internal class SecondaryStats
+    {
+       
+
+        public bool CalculateDodge() => new Random().NextDouble() < dodgeChance;
+        public bool CalculateParry() => new Random().NextDouble() < parryChance;
+        public bool CalculateBlock() => new Random().NextDouble() < blockChance;
+        public Damage CalculateBlockReduction(Damage aDamage)
+        {
+            if (CalculateBlock())
+            {
+                double reducedAmount = aDamage.DamageAmount - blockValue;
+                return new Damage(reducedAmount < 0 ? 0 : reducedAmount, aDamage.DamageType);
+            }
+            else return aDamage;
+        }
+        public double CalculateEHP(double playerHealth, double armorDamageReduction)
+        {
+            if (armorDamageReduction < 0 || armorDamageReduction >= 1)
+                throw new ArgumentOutOfRangeException(nameof(armorDamageReduction), "Damage reduction must be between 0 and 1 (exclusive).");
+
+            return playerHealth / (1 - armorDamageReduction);
+        }
+
+        public double CalculateDamageReduction(double armor, int attackerLevel)
+        {
+            double damageReduction;
+
+            if (attackerLevel < 60)
+            {
+                damageReduction = armor / (armor + 400 + 85 * attackerLevel);
+            }
+            else
+            {
+                damageReduction = armor / (armor + 400 + 85 * (attackerLevel + 4.5 * (attackerLevel - 59)));
+            }
+
+            return damageReduction;
+        }
+
+        double attackCriticalChance;
+        double attackCriticalDamage;
+        double perArmorPenetration;
+        int flatArmorPenetration;
+        double lifeSteal;
+        double bonusAttackHitChance;
+
+
+        double spellCriticalChance;
+        double spellCriticalDamage;
+        double perSpellPenetration;
+        int flatSpellPenetration;
+        double spellVamp;
+        double bonusSpellHitChance;
+
+        double healthRegen;
+        double dodgeChance;
+        double parryChance;
+        double blockChance;
+        double blockValue;
+
+
+        Armor armor;
+        SpellResitance spellResitance;
+        SpellDamage spellDamage;
+        WeaponSkill weaponSkill;
+        int defense;
+
+    }
+}
---
+++ Project 1/GameObjects/Unit/Stats/SpellResitance.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_1.GameObjects.Unit.Stats
+{
+    internal class SpellResitance
+    {
+        int baseResitance;
+        Dictionary<SpellSchools, int> resitanceBySchool;
+        public int BaseResitance => baseResitance;
+        public int[] ResitanceBySchool
+        {
+            get
+            {
+                int maxEnumValue = Enum.GetValues(typeof(SpellSchools)).Cast<int>().Max();
+                int[] result = new int[maxEnumValue + 1];
+
+                foreach (var kvp in resitanceBySchool)
+                {
+                    result[(int)kvp.Key] = kvp.Value;
+                }
+
+                return result;
+            }
+        }
+
+        public int GetResitance(SpellSchools aSchool) => baseResitance + (resitanceBySchool.TryGetValue(aSchool, out int schoolResitance) ? schoolResitance : 0);
+        public int GetResitance(SpellSchools[] aSchools, double[] aRatio) => (int)(baseResitance + (aSchools.Select((school, index) => (resitanceBySchool.TryGetValue(school, out int schoolResitance) ? schoolResitance : 0) * aRatio[index]).Sum()));
+        public SpellResitance(int aBaseResitance, Dictionary<SpellSchools, int> aResitanceBySchool)
+        {
+            baseResitance = aBaseResitance;
+            resitanceBySchool = aResitanceBySchool;
+        }
+        public SpellResitance(int aBaseResitance) : this(aBaseResitance, new Dictionary<SpellSchools, int>())
+        {
+
+        }
+        public SpellResitance() : this(0, new Dictionary<SpellSchools, int>())
+        {
+
+        }
+
+        [JsonConstructor]
+        public SpellResitance(int baseResitance, int[] resitanceBySchool) : this(baseResitance, new Dictionary<SpellSchools, int>())
+        {
+            for (int i = 0; i < resitanceBySchool.Length; i++)
+            {
+                this.resitanceBySchool[(SpellSchools)i] = resitanceBySchool[i];
+            }
+        }
+    }
+}
